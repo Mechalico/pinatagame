63dfcb23ad9a6b81c4bdef8fc05328fb2bdb67ba
mechalico <mechalicolabs@gmail.com>
2023-07-08 22:15:47 +0100

    Guard patrol behaviour

--- Assets/Scripts/Guard.cs
+++ Assets/Scripts/Guard.cs
@@ -1,32 +1,98 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Guard : MonoBehaviour
 {
-    public PlayerBehavior player;
-    public float speed;
+    GameObject playerObject;
+    PlayerBehavior player;
 
     public Rigidbody2D rb;
 
-    public Transform[] targets;
-    public int targetIndex;
+    public GameObject[] targets;
+    int targetIndex;
+
+    GuardState guardState;
+
+    Vector2 inputMovement;
+
+    float timeToArrive;
+    float timeWalked;
+    float rotationSpeed;
+    Vector3 rotationVelocity;
+
+    void Start()
+    {
+        guardState = GuardState.Patrol;
+        playerObject = GameObject.FindGameObjectWithTag("Player");
+        player = playerObject.GetComponent<PlayerBehavior>();
+
+        targetIndex = 0;
+        setNextWaypoint();
+    }
 
     void FixedUpdate()
     {
-        //turn around after reaching the last one
+        if (guardState == GuardState.Patrol)
+        {
+            timeWalked += Time.deltaTime;
+            rb.velocity = inputMovement / timeToArrive;
+            rb.rotation += (rotationSpeed * Time.deltaTime);
+            //move in that direction
+            if (timeWalked >= timeToArrive)
+            {
+                setNextWaypoint();
+            }
+
+        }
+
+    }
+
+    void setNextWaypoint()
+    {
+        var thisWaypoint = targets[targetIndex];
+        rb.position = thisWaypoint.transform.position;
+        rb.rotation = thisWaypoint.transform.rotation.eulerAngles.z;
+
+        Debug.Log(thisWaypoint.transform.rotation.eulerAngles.z);
+        Debug.Log("Set next waypoint");
+
+        targetIndex++;
+        timeWalked = 0;
+        //target initial point after reaching target point
         if (targetIndex >= targets.Length) targetIndex = 0;
+
+        var nextWaypoint = targets[targetIndex];
+        var nextWaypointScript = nextWaypoint.GetComponent<WaypointScript>();
 
         //get a vector pointing to your next waypoint.
         //this tells you direction and distance
-        Vector2 inputMovement = targets[targetIndex].position - transform.position;
+        inputMovement = (Vector2)nextWaypoint.transform.position - rb.position;
 
-        //if guards should smoothly turn around, that code would go here
+        if (nextWaypointScript.timeToArrive > 0)
+        {
+            timeToArrive = nextWaypointScript.timeToArrive;
 
-        //move in that direction
-        rb.velocity = inputMovement.normalized * speed * Time.deltaTime;
-        //when you reach a waypoint, turn around.
-        if (inputMovement.magnitude < 0.1) targetIndex += 1;
-        
+            if (nextWaypoint.transform.rotation.eulerAngles.z == rb.rotation)
+                rotationSpeed = 0;
+            else
+            {
+                if (nextWaypointScript.isClockwise)
+                    rotationSpeed = (nextWaypoint.transform.rotation.eulerAngles.z - rb.rotation) / nextWaypointScript.timeToArrive;
+                else
+                    rotationSpeed = (nextWaypoint.transform.rotation.eulerAngles.z - rb.rotation - 360) / nextWaypointScript.timeToArrive;
+            }
+
+            Debug.Log(rotationSpeed);
+        }
+    }
+
+    enum GuardState
+    {
+        Patrol,
+        Chase,
+        Stunned,
+        Reset
     }
 }
---
+++ Assets/Scripts/WaypointScript.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaypointScript : MonoBehaviour
+{
+    public float timeToArrive = 1;
+    public bool isClockwise;
+}
