8d2911a04734b1f0680997028982d709aa5f12f3
mechalico <mechalicolabs@gmail.com>
2023-07-09 17:52:53 +0100

    Added level 4 and how to play

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -21,6 +21,10 @@ public class Menu : MonoBehaviour
     {
         SceneManager.LoadScene("Level1");
     }
+    public void LoadHpwToPlay()
+    {
+        SceneManager.LoadScene("HowToPlay");
+    }
 
     public void QuitGame ()
     {
